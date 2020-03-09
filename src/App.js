e7925f9b6cba1283e5dbe7cef3892cfdde2ed494
EugeneShatalin <shatalin_ei@mail.ru>
2020-03-10 01:45:51 +0600

    We created an array whose elements are JSX elements. Map array method.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import TodoListTasks from './TodoListTasks.js';
 import TodoListFooter from './TodoListFooter.js';
 
 class App extends React.Component {
+    tasks = [
+        {title: "JS, ", isDone: true, priority: "priority: low"},
+        {title: "CSS, ", isDone: true, priority: "priority: high"},
+        {title: "HTML, ", isDone: true, priority: "priority: medium"},
+        {title: "ReactJS, ", isDone: false, priority: "priority: low"}];
+
+    filterValue = "All";
+
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader />
-                    <TodoListTasks />
-                    <TodoListFooter />
+                    <TodoListHeader/>
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue={this.filterValue}/>
                 </div>
             </div>
         );
     }
 }
-
 export default App;
 
